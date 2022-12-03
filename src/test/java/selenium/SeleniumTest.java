bd57bfecb90df88f1d17242ea88c34111cd3ee9e
dannymevs@hotmail.com <dannymevs@hotmail.com>
2022-12-03 00:35:33 -0500

    second commit

--- src/test/java/selenium/SeleniumTest.java
+++ src/test/java/selenium/SeleniumTest.java
@@ -1,2 +1,25 @@
-package selenium;public class SeleniumTest {
+package selenium;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class SeleniumTest {
+    @Test
+    public void automationTest() {
+        open("https://explorecalifornia.org/contact.htm");
+
+        $("#name").should(appear);
+        $("#comments").should(appear);
+
+        $("#name").setValue("Daniel Mevs");
+        $("#state").selectOptionContainingText("Florida");
+        $("#backpack").click();
+        $(By.name("newsletter")).selectRadio("yes");
+        $("#name").shouldHave(value("Daniel Mevs"));
+        String state = $("#state").getSelectedText();
+        assert(state).equals("Florida");
+        $("#backpack").shouldBe(selected);
+        $(By.name("newsletter")).shouldHave(value("yes"));
+    }
 }
